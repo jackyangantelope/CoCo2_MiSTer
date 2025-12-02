dcfba89c9f069c551ca571377848a416cdc53b0a
jackyangantelope <jack@retroremake.co>
2025-12-02 13:34:35 +0800

    modify frame

--- CoCo2.sv
+++ CoCo2.sv
@@ -57,6 +57,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -188,6 +190,8 @@ assign {DDRAM_CLK, DDRAM_BURSTCNT, DDRAM_ADDR, DDRAM_DIN, DDRAM_BE, DDRAM_RD, DD
 assign VGA_F1 = 0;
 assign VGA_DISABLE = 0;
 assign VGA_SCALER = 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 
 assign AUDIO_S = 0;
 assign AUDIO_L = AUDIO_R;
